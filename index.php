e35c0b9db694794b97cac6aee49e20088c60fd6d
sebastian <sebastianrios47@gmail.com>
2024-03-24 20:03:36 -0500

    Final_24/03/2024

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php include("cabecera.php"); ?>
+<?php
+include("cabecera.php");
+include("conexion.php");
+$objConexion = new conexion();
+$sqlSelect = "SELECT * FROM `proyectos`";
+$proyectos = $objConexion->consultar($sqlSelect);
+?>
+
+
 
 <div class="p-5 mb-4 bg-light rounded-3">
     <div class="container-fluid py-5">
@@ -9,6 +17,39 @@
 
     </div>
 </div>
+
+
+
+<div class="card-group">
+
+    <?php foreach ($proyectos as $proyecto) {  ?>
+
+
+        <div class="card" style="width: 18rem;">
+            <img src="img/<?php echo  $proyecto['imagen']; ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"> <?php echo $proyecto['nombre']; ?> </h5>
+                <p class="card-text"> <?php echo $proyecto['descripcion']; ?> </p>
+                <p class="card-text"><small class="text-body-secondary">****</small></p>
+            </div>
+        </div>
+
+
+
+
+        <!-- <div class="card">
+            <img src="img/<?php echo  $proyecto['imagen']; ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"> <?php echo $proyecto['nombre']; ?> </h5>
+                <p class="card-text"> <?php echo $proyecto['descripcion']; ?> </p>
+                <p class="card-text"><small class="text-body-secondary">****</small></p>
+            </div>
+        </div> -->
+
+    <?php } ?>
+
+</div>
+
 
 
 <?php include("pie.php"); ?>
